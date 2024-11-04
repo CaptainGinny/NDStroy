21c61520472843f3562bc49e99475f37a9d89b98
CaptainGinny <ginnymalko@gmail.com>
2024-11-04 10:19:01 +0300

    second commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,22 @@
-// import mobileNav from './modules/mobile-nav.js';
-// mobileNav();
+document.addEventListener('DOMContentLoaded', () => {
+  const openPopupBtn = document.getElementById('openPopupBtn');
+  const popup = document.getElementById('popup');
+  const closePopupBtn = document.querySelector('.close');
 
-// import isAvifWebp from 'avif-webp-checker';
-// isAvifWebp({ mode: 'webp' });
+  openPopupBtn.addEventListener('click', () => {
+    popup.style.display = 'flex';
+    document.body.classList.add('lock-scroll');
+  });
+
+  closePopupBtn.addEventListener('click', () => {
+    popup.style.display = 'none';
+    document.body.classList.remove('lock-scroll');
+  });
+
+  popup.addEventListener('click', (e) => {
+    if (e.target === popup) {
+      popup.style.display = 'none';
+      document.body.classList.remove('lock-scroll');
+    }
+  });
+});
